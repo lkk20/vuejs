b050a03df95af5756f8237f971097601d822acf8
lkk <152.com>
2020-05-30 17:33:47 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components:{
+	  
+  },
+  data(){
+	  return{
+		 
+	  }
+  },
+  methods:{
+  	  
+  }
+}
+</script>
+
+<style>
+
+</style>
